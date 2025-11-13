43669fb53ff766e87ce8dc5c52922a45356ef6ce
corybey <corybey0000@gmail.com>
2025-11-13 03:16:18 -0500

    Model and Service Builds

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HW2 Message Board',
+      home: const FirebaseTestScreen(),
+    );
+  }
+}
+
+class FirebaseTestScreen extends StatelessWidget {
+  const FirebaseTestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Firebase is initialized ✅',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
